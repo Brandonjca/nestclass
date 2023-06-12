07fe96b8f229933f2b91434af02be2e8e9badea5
SANTIAGO_DAVID_GUALLICHICO_QUISHPE <sdq.guallichico@yavirac.edu.ec>
2023-06-12 10:41:10 -0500

    relaciones

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,31 @@
 export class Product {}
+// 
+// 
+// @OneToOne (()=>RolEntity, rol=>rol.user ) 
+// @JoinColumn()
+// rol:RolEntity
+// 
+// 
+// @OneToOne(()=>UserEntity, user=>user.rol)
+//@JoinColumn()
+//user:UserEntity
+// 
+// @ManyToMany(()=>ProductEntity, product=>product.size)
+// product:ProductEntity[]
+// 
+// 
+// @ManyToMany(()=>SizesEntity, sizes=>sizes.product)
+// @JoinTable()
+// sizes:SizesEntity[]
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
